520cb7817fff44db2b403cac1f50021c7d093d2c
Danny <dyaroslavski@gmail.com>
2014-11-01 16:24:18 -0700

    pay user and submit score

--- js/logic.js
+++ js/logic.js
@@ -5,29 +5,51 @@ function receivedData(response){
 }
 
 // submit score for game
-function submitScore(name, gameid, newscore){
-    var scores = new Firebase("https://moneymoney.firebaseio.com/games/"+gameid+"/scores/"+name+"/score");
-    scores.transaction(function(score){
-        if(newscore>score){
-            return newscore;
+function saveRun(name, gameid, newscore, rundata){
+    var currgame = new Firebase("https://moneymoney.firebaseio.com/games/"+gameid);
+    currgame.transaction(function(game){
+        if(game==null){
+            return game;
         }
-        return score;
+        if(game["scores"]==null || game["scores"][name]==null){
+            return game;
+        }
+
+        if(newscore>game["scores"][name]["score"]){
+            game["scores"][name]["score"]=newscore;
+            game["scores"][name]["rundata"]=rundata;
+            return game;
+        }
     });
 }
 
-//payUser("Danny",1);
 function payUser(name, gameid){
-    var coins = new Firebase("https://moneymoney.firebaseio.com/users/"+name+"/coins");
-    coins.transaction(function(coins){
-        if(coins==null){
-            return 0;
-        }
-        if(coins>0){
-            return coins-1;
-        }
+    var newfb = new Firebase("https://moneymoney.firebaseio.com/");
+        newfb.transaction(function(topfb){
+            if(topfb==null){
+                return {}
+            }
+            if(topfb["users"][name]["coins"]>0 && topfb["games"][gameid]!=null){
+                if(topfb["games"][gameid]["scores"]==null){
+                    topfb["games"][gameid]["scores"]={};
+                } else if(topfb["games"][gameid]["scores"][name]!=null){
+                    return topfb;
+                }
+                topfb["users"][name]["coins"]--;
+                topfb["games"][gameid]["scores"][name]={score:0, rundata:""};
+                return topfb;
+            }
     });
 }
+
 
 function payWinners(){
 }
+
+
+//example:
+//user pays, user can then save a run
+payUser("Danny",1);
+saveRun("Danny",1,210,"adsdfsdff");
+
 
