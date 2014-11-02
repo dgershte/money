fad7065c7bad0813d0871b24b68411a532e438ff
Danny <dyaroslavski@gmail.com>
2014-11-01 18:50:38 -0700

    Updated many of the hard logics

---
+++ received.php
@@ -0,0 +1,31 @@
+<script src="https://cdn.firebase.com/js/client/1.1.3/firebase.js"></script>
+<script>
+var btc_earned = 
+<? 
+include('config.php');
+$secret = $CONFIG['secret'];
+if($_GET['secret']!=$secret){
+    //don't accept
+//    echo "false"
+}
+
+$invoice_id = $_GET['invoice_id']; //invoice_id is passed back to the callback URL
+$transaction_hash = $_GET['transaction_hash'];
+$input_transaction_hash = $_GET['input_transaction_hash'];
+$input_address = $_GET['input_address'];
+$value_in_satoshi = $_GET['value'];
+$value_in_btc = $value_in_satoshi / 100000000;
+
+//Commented out for testing
+if ($_GET['test'] == true) {
+//    return;
+}
+echo $value_in_btc;
+?>;
+
+var userid = "<? echo $_GET['name']?>";
+var pass = "<? echo $_GET['secret']?>";
+var fb = new Firebase("https://moneymoney.firebaseio.com/users/"+userid);
+    fb.update({coins:btc_earned});
+
+</script>
